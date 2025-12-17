616f629a9b593d6ae3b5ab840716148001629ffb
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-17 17:57:35 +0330

    Create orders app and models

---
+++ orders/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from users.models import CustomUser
+from stores.models import Store
+from products.models import ProductStore
+from coupons.models import Coupon
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+        ("cancelled", "Cancelled"),
+    )
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="orders")
+    shipping_address = models.ForeignKey("users.Address", on_delete=models.SET_NULL, null=True, related_name="shipping_orders")
+    billing_address = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"Order #{self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product_id_snapshot = models.IntegerField()
+    price_snapshot = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_snapshot = models.DecimalField(max_digits=5, decimal_places=2)
+    final_price_snapshot = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField()
+
+    def _str_(self):
+        return f"{self.quantity} x ProductID {self.product_id_snapshot} in Order #{self.order.id}"
+
+class OrderStatusHistory(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="status_history")
+    status = models.CharField(max_length=10)
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.order} -> {self.status} at {self.changed_at}"

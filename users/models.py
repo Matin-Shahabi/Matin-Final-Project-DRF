995f4660c124fc6c257f65c96028ba972c3dfaad
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:45:53 +0330

    Created: user app models

--- users/models.py
+++ users/models.py
@@ -12,19 +12,39 @@ class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10, null=True, blank=True)
     phone = models.CharField(max_length=20, unique=True)
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        null=True,
+        blank=True
+    )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="guest")
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.username} ({self.email})"
 
 class Address(models.Model):
+    LABEL_CHOICES = (
+        ("home", "Home"),
+        ("work", "Work"),
+        ("office", "Office"),
+        ("other", "Other"),
+    )
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="addresses")
+    label = models.CharField(
+        max_length=20,
+        choices=LABEL_CHOICES,
+        default="home"
+    )
+    country = models.CharField(
+        max_length=100,
+        default="Iran"
+    )
     province = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     address = models.TextField()
     postal_code = models.CharField(max_length=20)
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.user.username} - {self.city}, {self.province}"
 
 class OTP(models.Model):
@@ -33,5 +53,5 @@ class OTP(models.Model):
     expires_at = models.DateTimeField()
     is_used = models.BooleanField(default=False)
 
-    def _str_(self):
+    def __str__(self):
         return f"OTP {self.code} for {self.user.username} ({'Used' if self.is_used else 'Unused'})"

1bede818040c952b56a3f68b88292d33bfab817f
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:49:15 +0330

    Created: coupons app models

--- coupons/models.py
+++ coupons/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
-    expiration_date = models.DateField()
-    usage_limit = models.IntegerField(default=1)
+    DISCOUNT_TYPE_CHOICES = (
+        ("percent", "Percent"),
+        ("fixed", "Fixed"),
+    )
 
-    def _str_(self):
-        return self.code
+    code = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    discount_type = models.CharField(
+        max_length=10,
+        choices=DISCOUNT_TYPE_CHOICES,default="percent"
+    )
+
+    discount_value = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,default=0.00
+    )
+
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(null=True,blank=True)
+
+    is_active = models.BooleanField(default=True)
+
+    usage_limit = models.PositiveIntegerField()
+    usage_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.code} ({self.discount_type})"

816ab757eecc17f32aced74cdc016179a0ea30cb
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:52:55 +0330

    Created: serializers.py and urls.py

---
+++ categories/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "name",
+            "description",
+            "image",
+            "parent",
+            "children",
+        ]
+
+    def get_children(self, obj):
+        children = obj.children.filter(is_active=True)
+        return CategorySerializer(children, many=True).data

a5efbea968ea283b5b2e649d078a92c56c251325
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:46:49 +0330

    Created: categories app models

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(max_length=300,default="No Description yet")
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(
+        upload_to="categories/",
+        null=True,
+        blank=True
+    )
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.SET_NULL, related_name="children")
 
-    def _str_(self):
+    def __str__(self):
         return self.name

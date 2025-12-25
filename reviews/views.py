1c8414012c3ef5e30a58650e2de9d533241c57d2
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:57:51 +0330

    Created & Edited: reviews view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListCreateAPIView
+from .models import Review
+from .serializers import ReviewListSerializer, ReviewCreateSerializer
 
-# Create your views here.
+class ReviewPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+
+
+
+class ReviewListCreateAPIView(ListCreateAPIView):
+    queryset = Review.objects.all().order_by("-id")
+    pagination_class = ReviewPagination
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return ReviewCreateSerializer
+        return ReviewListSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user if self.request.user.is_authenticated else None
+        serializer.save(user=user)

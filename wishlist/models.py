1fc13ade1ba0e23ebf3995b5fb1a33cb9346766a
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:49:55 +0330

    Created: wishlist app models

--- wishlist/models.py
+++ wishlist/models.py
@@ -5,12 +5,12 @@ from products.models import Product
 class Wishlist(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="wishlists")
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.user.username}'s Wishlist"
 
 class WishlistItem(models.Model):
     wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.product.name} in {self.wishlist.user.username}'s wishlist"

69528311395af6807b71de5c6c8d8c3666d96a19
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:57:30 +0330

    Created & Edited: products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView,RetrieveAPIView,CreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .models import Product
+from .serializers import ProductListSerializer,ProductDetailSerializer
+from rest_framework.permissions import AllowAny
+from reviews.models import Review
+from reviews.serializers import ReviewCreateSerializer
 
-# Create your views here.
+
+
+
+class ProductListAPI(ListAPIView):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductListSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    
+    filterset_fields = {
+        "category__id": ["exact"],
+        "price": ["gte", "lte"],
+        "name": ["icontains"],
+    }
+    ordering_fields = ["price", "rating"]
+    ordering = ["-rating", "-price"]
+
+
+
+class ProductDetailAPIView(RetrieveAPIView):
+    queryset = Product.objects.filter(is_active=True)
+    serializer_class = ProductDetailSerializer
+
+

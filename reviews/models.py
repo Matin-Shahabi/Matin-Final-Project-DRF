b3c19c8fe46ad1b8782f07ad3db12cffba8edc59
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-25 21:49:33 +0330

    Created: reviews app models

--- reviews/models.py
+++ reviews/models.py
@@ -3,10 +3,11 @@ from users.models import CustomUser
 from products.models import Product
 
 class Review(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.FloatField()
     comment = models.TextField()
 
-    def _str_(self):
-        return f"Review by {self.user.username} for {self.product.name} - {self.rating}/5"
+    def __str__(self):
+        return f"Review by {self.user.username if self.user else 'Anonymous'} for {self.product.name} - {self.rating}/5"

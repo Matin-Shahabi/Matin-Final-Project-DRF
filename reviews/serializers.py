c38a5375e5ac26b5d199b600b87b821b15ca7bed
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:50:35 +0330

    Edited: serializers

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,29 +1,82 @@
+# reviews/serializers.py
 from rest_framework import serializers
-from .models import Review
+from .models import StoreReview,Review as ProductReview
+from products.models import Product
+from stores.models import Store
 
-class ReviewListSerializer(serializers.ModelSerializer):
-    user_name = serializers.SerializerMethodField()
+class UnifiedReviewSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    type = serializers.ChoiceField(choices=['product', 'store'], write_only=True)
+    target_id = serializers.IntegerField(write_only=True)  # id محصول یا فروشگاه
+    rating = serializers.FloatField(min_value=0.5, max_value=5.0)
+    comment = serializers.CharField(max_length=1000, allow_blank=True, required=False)
+    created_at = serializers.DateTimeField(read_only=True)
 
-    class Meta:
-        model = Review
-        fields = ['id', 'product', 'rating', 'comment', 'user_name', 'created_at']
+    # فیلدهای نمایشی
+    target_name = serializers.CharField(read_only=True)
+    target_image = serializers.ImageField(read_only=True, allow_null=True)
 
-    def get_user_name(self, obj):
-        if obj.user:
-            return obj.user.username
-        return "Anonymous"
+    def validate(self, data):
+        type_ = data.get('type')
+        target_id = data.get('target_id')
 
+        if type_ == 'product':
+            if not Product.objects.filter(id=target_id, is_active=True).exists():
+                raise serializers.ValidationError("محصول یافت نشد.")
+        elif type_ == 'store':
+            if not Store.objects.filter(id=target_id, is_active=True).exists():
+                raise serializers.Validation("فروشگاه یافت نشد.")
+        else:
+            raise serializers.ValidationError("نوع نظر باید product یا store باشد.")
 
-class ReviewCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ['product', 'rating', 'comment']
+        return data
 
-    def perform_create(self, serializer):
-        user = self.request.user if self.request.user.is_authenticated else None
-        serializer.save(user=user)
+    def create(self, validated_data):
+        user = self.context['request'].user
+        type_ = validated_data.pop('type')
+        target_id = validated_data.pop('target_id')
 
-    def validate_rating(self, value):
-        if value < 0 or value > 5:
-            raise serializers.ValidationError("Rating must be between 0 and 5")
-        return value
+        if type_ == 'product':
+            product = Product.objects.get(id=target_id)
+            review = ProductReview.objects.create(user=user, product=product, **validated_data)
+        else:
+            store = Store.objects.get(id=target_id)
+            review = StoreReview.objects.create(user=user, store=store, **validated_data)
+
+        return review
+
+    def update(self, instance, validated_data):
+        rating = validated_data.get('rating')
+        comment = validated_data.get('comment')
+
+        if rating is not None:
+            instance.rating = rating
+        if comment is not None:
+            instance.comment = comment
+
+        instance.save()
+        return instance
+
+    def to_representation(self, instance):
+        # نمایش یکپارچه برای هر دو نوع نظر
+        data = {
+            'id': instance.id,
+            'rating': instance.rating,
+            'comment': instance.comment,
+            'created_at': instance.created_at,
+        }
+
+        if hasattr(instance, 'product'):  # نظر محصول
+            data['type'] = 'product'
+            data['target_id'] = instance.product.id
+            data['target_name'] = instance.product.name
+            first_image = instance.product.images.first()
+            data['target_image'] = first_image.image.url if first_image else None
+
+        elif hasattr(instance, 'store'):  # نظر فروشگاه
+            data['type'] = 'store'
+            data['target_id'] = instance.store.id
+            data['target_name'] = instance.store.name
+            data['target_image'] = instance.store.logo.url if instance.store.logo else None
+
+        return data

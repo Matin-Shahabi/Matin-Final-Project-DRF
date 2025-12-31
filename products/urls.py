7d97b55acca1a199adf0a873dcef4cbd1f308d3d
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:49:46 +0330

    Edited: urls

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ProductListAPI,ProductDetailAPIView
+from .views import (
+    ProductListView, ProductDetailView,
+    ReviewCreateView, ReviewListView
+)
+
 
 urlpatterns = [
-    path("", ProductListAPI.as_view(), name="product-list"),
-    path('<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
-
+    path('', ProductListView.as_view(), name='product-list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('<int:pk>/review_create/', ReviewCreateView.as_view(), name='product-review-create'),
+    path('<int:pk>/review_list/', ReviewListView.as_view(), name='product-review-list'),
 ]
-

283b9648d28158ff4b0e900a5ae24fbec80585a2
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:47:37 +0330

    Edited: model

--- reviews/models.py
+++ reviews/models.py
@@ -1,13 +1,25 @@
 from django.db import models
-from users.models import CustomUser
+from users.models import CustomUser,BaseModel
 from products.models import Product
+from stores.models import Store
 
-class Review(models.Model):
+class Review(BaseModel):
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
     rating = models.FloatField()
     comment = models.TextField()
 
     def __str__(self):
         return f"Review by {self.user.username if self.user else 'Anonymous'} for {self.product.name} - {self.rating}/5"
+    
+
+
+class StoreReview(BaseModel):
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.FloatField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review by {self.user.username if self.user else 'Anonymous'} for {self.store.name} - {self.rating}/5"

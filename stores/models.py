ef1290c099fa70b43304ff9ecb8b562c8420428d
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:47:08 +0330

    Edited: model

--- stores/models.py
+++ stores/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from users.models import CustomUser
+from users.models import CustomUser,BaseModel
 
-class Store(models.Model):
+class Store(BaseModel):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="stores")
     name = models.CharField(max_length=255)
     address = models.TextField()

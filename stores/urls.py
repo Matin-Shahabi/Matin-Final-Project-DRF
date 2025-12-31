da6f00777aea834d674b9183547ce093f1b03df9
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:49:12 +0330

    Edited: urls

--- stores/urls.py
+++ stores/urls.py
@@ -1,7 +1,17 @@
 
 from django.urls import path
-from .views import ProductStoreListCreateAPIView
+from .views import *
 
 urlpatterns = [
-    path("items/", ProductStoreListCreateAPIView.as_view(), name="store-items-list"),
+    # عمومی
+    path('', StoreListView.as_view(), name='store-list'),
+    path('<int:pk>/', StoreDetailView.as_view(), name='store-detail'),
+    path('<int:pk>/products/', StoreProductsPublicView.as_view(), name='store-products'),
+    path('<int:pk>/review_list/', StoreReviewListView.as_view(), name='store-review-list'),
+    path('<int:pk>/review_create/', StoreReviewCreateView.as_view(), name='store-review-create'),
+
+    # مدیریت توسط فروشنده
+    path('items/', MyStoreItemsListCreateView.as_view(), name='my-store-items'),
+    path('items/<int:pk>/', MyStoreItemDetailView.as_view(), name='my-store-item-detail'),
 ]
+

baaba0e3cdedbd891d989208e5d6665e3c2ebc54
Matin-Shahabi <matio.mini8374@gmail.com>
2025-12-31 20:51:57 +0330

    Edited: urls of user

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+from .views import RequestOTPView, VerifyOTPView, RegisterView, LoginView,MyUserView, AddressListCreateView,AddressDetailView, RegisterAsSellerView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
+urlpatterns = [
+    path('accounts/request-otp/', RequestOTPView.as_view(), name='request-otp'),
+    path('accounts/verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
+    path('accounts/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/login/', LoginView.as_view(), name='login'),  # alias برای verify-otp
+    path('myuser/', MyUserView.as_view(), name='myuser'),
+    path('myuser/address/', AddressListCreateView.as_view(), name='address-list-create'),
+    path('myuser/address/<int:pk>/', AddressDetailView.as_view(), name='address-detail'),
+    path('myuser/register_as_seller/', RegisterAsSellerView.as_view(), name='register-as-seller'),
+]
+
+
